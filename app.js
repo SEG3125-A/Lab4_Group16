90321e71eb338eed7b0d44ca8f0e7dd550f9ce20
CHasanJ <114039205+CHasanJ@users.noreply.github.com>
2024-02-08 20:16:24 -0500

    Added the staff section of the website and the selection of staff in the booking pop up

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    document.querySelector('.btn-primary[data-target="#bookingModal"]').addEventListener('click', function () {
-
-        document.querySelector('#serviceName').value = this.getAttribute('data-service');
+    document.querySelectorAll('.btn-primary[data-target="#bookingModal"]').forEach(function (button) {
+        button.addEventListener('click', function () {
+            document.querySelector('#serviceName').value = this.getAttribute('data-service');
+            document.querySelector('#barberName').value = this.getAttribute('data-barber');
+            var isHaircutStyle = this.getAttribute('data-service').includes('Haircut');
+            $('#serviceName').prop('readonly', isHaircutStyle);
+        });
     });
 
-
     document.querySelector('#bookingModal .btn-primary').addEventListener('click', function () {
-
         if (validateBookingForm()) {
             submitBookingForm();
         }
     });
 
-
     function validateBookingForm() {
+        let barberName = document.querySelector('#barberName').value;
         let serviceName = document.querySelector('#serviceName').value;
         let selectedDate = document.querySelector('#bookingDate').value;
         let selectedTime = document.querySelector('#bookingTime').value;
         let userName = document.querySelector('#userName').value;
         let userEmail = document.querySelector('#userEmail').value;
 
-
-        if (serviceName && selectedDate && selectedTime && userName && userEmail) {
+        if (barberName && serviceName && selectedDate && selectedTime && userName && userEmail) {
             return true;
         } else {
             alert('Please fill in all the required fields.');
@@ -30,10 +31,9 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-
     function submitBookingForm() {
-
         let formData = {
+            barberName: document.querySelector('#barberName').value,
             serviceName: document.querySelector('#serviceName').value,
             date: document.querySelector('#bookingDate').value,
             time: document.querySelector('#bookingTime').value,
@@ -43,7 +43,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
         console.log('Form submitted:', formData);
 
-
         $('#bookingModal').modal('hide');
     }
+
+    $('[data-target="#bookingModal"]').on('click', function () {
+        var serviceName = $(this).data('service');
+        $('#serviceName').val(serviceName);
+
+        var barberName = $(this).data('barber');
+        $('#barberName').val(barberName);
+
+        var isHaircutStyle = serviceName.includes('Haircut');
+        $('#serviceName').prop('readonly', isHaircutStyle);
+
+        var isBarberName = barberName.includes('Barber');
+        $('#barberName').prop('readonly', isBarberName);
+
+        var isStaffSection = $(this).closest('#staff').length > 0;
+
+        if (isStaffSection) {
+            $('#serviceDropdownGroup').show();
+            $('#serviceNameInputGroup').hide();
+        } else {
+            $('#serviceDropdownGroup').hide();
+            $('#serviceNameInputGroup').show();
+        }
+    });
+
 });
+
